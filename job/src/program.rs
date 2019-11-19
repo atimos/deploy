b03b183c570c335013342521b7f58cf17e7c601d
Alexander Seppälä <dev@timot.se>
2019-11-19 08:46:04 +0100

    split program into program and reference and let Programs handle state

--- job/src/program.rs
+++ job/src/program.rs
@@ -4,7 +4,7 @@ use pipeline::{data::Template, Arguments, Command, InstanceId, Location, Node};
 use std::collections::HashMap;
 
 pub struct Programs {
-    inner: HashMap<InstanceId, Program>,
+    inner: HashMap<InstanceId, (Reference, Option<Program>)>,
 }
 
 pub struct Output {
@@ -35,40 +35,34 @@ impl Programs {
     ) -> Result<Output, Error> {
         let program =
             self.inner.get_mut(id).ok_or_else(|| Error::UnknownInstance(id.to_owned()))?;
-        program.load(args, env)?;
-        program.run(cmds, args, env)
+        Ok(Output { err: Vec::new(), ok: Vec::new(), status: Status::Success })
+        // program.load(args, env)?;
+        // program.run(cmds, args, env)
+    }
+}
+
+#[derive(Debug)]
+pub enum Reference {
+    Wasm { uri: Template },
+    Oci { repository: Template, image: Template },
+}
+
+impl Reference {
+    fn load(self, args: &Option<Arguments>, env: &Environment) -> Result<Program, Error> {
+        match self {
+            Self::Wasm { uri } => Ok(Program::Wasm { binary: Vec::new() }),
+            Self::Oci { repository, image } => Ok(Program::Oci { container: String::new() }),
+        }
     }
 }
 
 #[derive(Debug)]
 pub enum Program {
-    Wasm { uri: Template, binary: Option<Vec<u8>> },
-    Oci { repository: Template, image: Template, container: Option<String> },
+    Wasm { binary: Vec<u8> },
+    Oci { container: String },
 }
 
 impl Program {
-    fn load(&mut self, args: &Option<Arguments>, env: &Environment) -> Result<(), Error> {
-        match self {
-            Self::Wasm { binary: None, uri } => {
-                let uri = uri.to_owned();
-                std::mem::replace(self, Self::Wasm { binary: Some(Vec::new()), uri });
-                Ok(())
-            }
-            Self::Oci { repository, image, container: None } => {
-                let image = image.to_owned();
-                let repository = repository.to_owned();
-
-                std::mem::replace(self, Self::Oci {
-                    container: Some(String::new()),
-                    image,
-                    repository,
-                });
-                Ok(())
-            }
-            _ => Ok(()),
-        }
-    }
-
     fn run(
         &self,
         cmd: &[Command],
@@ -79,17 +73,16 @@ impl Program {
     }
 }
 
-fn get_programs(node: &Node, programs: &mut HashMap<InstanceId, Program>) {
+fn get_programs(node: &Node, programs: &mut HashMap<InstanceId, (Reference, Option<Program>)>) {
     match node {
         Node::Commands { location, id, .. } => {
-            programs.insert(id.to_owned(), match location {
-                Location::Wasm { uri } => Program::Wasm { uri: uri.to_owned(), binary: None },
-                Location::Oci { repository, image } => Program::Oci {
+            programs.insert(id.to_owned(), (match location {
+                Location::Wasm { uri } => Reference::Wasm { uri: uri.to_owned() },
+                Location::Oci { repository, image } => Reference::Oci {
                     repository: repository.to_owned(),
                     image: image.to_owned(),
-                    container: None,
                 },
-            });
+            }, None));
         }
         Node::Nodes { nodes, .. } => nodes.iter().for_each(|node| get_programs(node, programs)),
     }
